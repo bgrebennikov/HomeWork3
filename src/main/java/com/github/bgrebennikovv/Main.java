def9c0f36d69b3304979b745a2d0bcf25994163f
Boris <bgrebennikovv@gmail.com>
2024-12-04 13:17:30 +0400

    Task 1

--- src/main/java/com/github/bgrebennikovv/Main.java
+++ src/main/java/com/github/bgrebennikovv/Main.java
@@ -2,6 +2,30 @@ package com.github.bgrebennikovv;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1
+        int intVar;
+        byte byteVar;
+        short shortVar;
+        long longVar;
+        float floatVar;
+        double doubleVar;
+
+        intVar = 11556975;
+        byteVar = 127;
+        shortVar = 32000;
+        longVar = 8456984463215648543L;
+        floatVar = 165.21f;
+        doubleVar = 1.7E+30;
+
+
+        System.out.printf("Значение переменной intVar с типом \"int\" равно %s\n", intVar);
+        System.out.printf("Значение переменной byteVar с типом \"byte\" равно %s\n", byteVar);
+        System.out.printf("Значение переменной shortVar с типом \"short\" равно %s\n", shortVar);
+        System.out.printf("Значение переменной longVar с типом \"long\" равно %s\n", longVar);
+        System.out.printf("Значение переменной floatVar с типом \"float\" равно %s\n", floatVar);
+        System.out.printf("Значение переменной doubleVar с типом \"double\" равно %s\n\n", doubleVar);
+
+
     }
 }
